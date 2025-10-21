a950f620681411f60bd489f7e71305d0808e8689
David Ruiz <tu-email@ejemplo.com>
2025-10-21 00:12:26 -0500

    Actualización de archivos del proyecto

--- src/main/java/com/co/gestiondecitasmedicas/repository/ClinicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.co.gestiondecitasmedicas.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.co.gestiondecitasmedicas.models.Clinica;
-
-public interface ClinicaRepository extends JpaRepository<Clinica, Integer> {
-    Optional<Clinica> findByUsuarioId(Integer usuarioId);
-}
